57b5dca2a37e795ddc8373c38044454632a70458
madhukar-pdl <madhukar.raj@peapoddigitallabs.com>
2023-06-15 18:55:57 -0500

    css changes

--- src/components/ChatOptions.js
+++ src/components/ChatOptions.js
@@ -9,14 +9,6 @@ function ChatOptions() {
        option= "I want to make a Dish"
        id= "MAKE_DISH"
       />
-      <ChatOptionButton
-       option= "What can i make from my orders ?"
-       id= "MAKE_DISH_FROM_ORDER"
-      />
-      <ChatOptionButton
-       option= "what can i make from a list of ingredients?"
-       id= "MAKE_DISH_FROM_ITEMS"
-      />
     </div>
   );
 }
